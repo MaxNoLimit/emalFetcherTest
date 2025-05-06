8d4e2614a15e3191012b2140da9c436ef03ce350
MaxNoLimit <maxnolimit@MaxNoLimit-Legion.>
2025-05-06 16:49:03 +0700

    need to work on google credentials config

--- emailFetcher.py
+++ emailFetcher.py
@@ -1,12 +1,24 @@
 # from imaplib import IMAP4_SSL
 import getpass, imaplib
 
-
+imap_host = 'imap.gmail.com'  # Replace with the target email address
 
 if __name__ == "__main__":
-    emailHandler = imaplib.IMAP4_SSL('imap.gmail.com')
-    emailHandler.login(getpass.getuser(), getpass.getpass())
-    emailHandler.select('inbox')
+    # Connect to the email server
+    emailHandler = imaplib.IMAP4_SSL(imap_host)
+
+    # getpass input for user
+    userVar = input("Enter your email address: ")  # Prompt for the email address
+
+    # getpass input for password
+    passwordVar = getpass.getpass()  # Prompt for the password
+
+    # Login to the email account
+    print("Logging in with %s and %s" % (userVar, passwordVar))
+    emailHandler.login(userVar, passwordVar)
+
+
+    emailHandler.select('Inbox')
     typ, data = emailHandler.search(None, 'ALL')
     for num in data[0].split():
         typ, data = emailHandler.fetch(num, '(RFC822)')
