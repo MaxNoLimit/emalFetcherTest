0053d59ffc4e9b3fdcf6129fe8c98b2088f40ddc
MaxNoLimit <maxnolimit@MaxNoLimit-Legion.>
2025-05-08 16:09:43 +0700

    can accept subject input for fetching a certain email's subject

--- emailFetcher.py
+++ emailFetcher.py
@@ -2,8 +2,6 @@
 import getpass, imaplib
 from imap_tools import MailBox, AND
 
-
-
 imap_host = 'imap.gmail.com'  # Replace with the target email address
 
 def fetch_email_with_imaplib():
@@ -35,7 +33,7 @@ def fetch_email_with_imaplib():
     emailHandler.close()
     emailHandler.logout()
 
-def fetch_email_with_imap_tools():
+def fetch_email_with_imap_tools_specific_subject():
     # perform input username and password
     userVar, passwordVar = login_email()
 
@@ -62,6 +60,33 @@ def fetch_email_with_imap_tools():
             for atth in msg.attachments:
                 print(atth.filename)
                 print(atth.content_type)
+                
+def fetch_email_with_imap_tools():
+    # perform input username and password
+    userVar, passwordVar = login_email()
+    wantedSubject = input("Subject (might be a keyword): ")
+
+    # save login result
+    tempMailBox = MailBox(imap_host).login(userVar, passwordVar)
+    # Print the login result
+    if tempMailBox.login_result[0] != 'OK':
+        # if login failed, print the error message
+        print("Login failed: %s" % (tempMailBox.login_result[1]))
+        exit(1)
+
+    # If login is successful, print the success message
+    print("Login successful!")
+    print("fetching email pls wait...: " + wantedSubject)
+    
+    # new method
+    # selectedMail = tempMailBox.fetch(AND(subject='Lamaran Posisi Data Scientist'))
+    for selectedMail in tempMailBox.fetch(AND(subject=wantedSubject)):
+    # for selectedMail in tempMailBox.fetch(AND(subject='Lamaran Posisi')):
+        print(selectedMail.from_, selectedMail.date_str)
+        print(selectedMail.text)
+        for selectedAttach in selectedMail.attachments:
+            print(selectedAttach.filename)
+            print(selectedAttach.content_type)
 
 def login_email():
     # getpass input for user
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import emailFetcher as eF
 
 if __name__ == "__main__":
     eF.fetch_email_with_imap_tools()
+    print("Done!")
