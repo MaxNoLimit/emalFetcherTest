c3c4f0e5736e4da65d73651d1fa60627d560e141
MaxNoLimit <maxnolimit@MaxNoLimit-Legion.>
2025-05-08 18:46:55 +0700

    tidying code but outlook method still failed to login

--- emailFetcher.py
+++ emailFetcher.py
@@ -1,43 +1,18 @@
-import getpass, imaplib, os
+import getpass, os
 from imap_tools import MailBox, AND
 
-imap_host = 'imap.gmail.com'  # Replace with the target email address
+imap_host_gmail = 'imap.gmail.com'  # Replace with the target email address
+imap_host_outlook = 'outlook.office365.com'
+                
+def fetch_email_with_imap_tools_gmail():
+    # print debug message
+    print("Using gmail")
 
-def fetch_email_with_imaplib():
-    # Connect to the email server
-    emailHandler = imaplib.IMAP4_SSL(imap_host)
-
-    # perform input username and password
-    userVar, passwordVar = login_email()
-
-    # Login to the email account
-    print("Logging in into %s" % (userVar))
-    loginResult = emailHandler.login(userVar, passwordVar)
-
-    # Print the login result
-    if loginResult[0] != 'OK':
-        # if login failed, print the error message
-        print("Login failed: %s" % (loginResult[1]))
-        exit(1)
-
-    # If login is successful, print the success message
-    print("Login successful!")
-
-    # Select Inbox
-    emailHandler.select('Inbox')
-
-    tempFetchedMsg = emailHandler.fetch()
-    
-
-    emailHandler.close()
-    emailHandler.logout()
-
-def fetch_email_with_imap_tools_specific_subject():
     # perform input username and password
     userVar, passwordVar = login_email()
 
     # save login result
-    tempMailBox = MailBox(imap_host).login(userVar, passwordVar)
+    tempMailBox = MailBox(imap_host_gmail).login(userVar, passwordVar)
     # Print the login result
     if tempMailBox.login_result[0] != 'OK':
         # if login failed, print the error message
@@ -46,32 +21,36 @@ def fetch_email_with_imap_tools_specific_subject():
 
     # If login is successful, print the success message
     print("Login successful!")
-    print("fetching email pls wait...")
+    wantedSubject = input("Subject (might be a keyword): ")
+    print("fetching email pls wait...: " + wantedSubject)
     
-    # start looping to search a certain email
-    nCount = 0
-    for msg in tempMailBox.fetch():
-        nCount += 1
-        print(nCount)
-        if msg.subject == 'Lamaran Posisi Data Scientist – William Deli':
-            print(msg.from_, msg.date_str)
-            print(msg.text)
-            for atth in msg.attachments:
-                print(atth.filename)
-                print(atth.content_type)
-                
-def fetch_email_with_imap_tools():
     # making download attachment folder
     SAVE_DIR ="download_attachment"
     os.makedirs(SAVE_DIR, exist_ok=True)
     print(f"Attachments will be saved to: {os.path.abspath(SAVE_DIR)}")
     
+    for selectedMail in tempMailBox.fetch(AND(subject=wantedSubject)):
+        print(selectedMail.from_, selectedMail.date_str)
+        # print(selectedMail.text)
+        for selectedAttach in selectedMail.attachments:
+            print(selectedAttach.filename)
+            print(selectedAttach.content_type)
+            file_path = os.path.join(SAVE_DIR, selectedAttach.filename)
+            print(f"  Downloading: {selectedAttach.filename} to {file_path}")
+            
+            with open(file_path, 'wb') as f:
+                f.write(selectedAttach.payload)
+                print("    Downloaded successfully.")
+                
+def fetch_email_with_imap_tools_outlook():
+    # print debug message
+    print("Using outlook")
+    
     # perform input username and password
     userVar, passwordVar = login_email()
-    wantedSubject = input("Subject (might be a keyword): ")
 
     # save login result
-    tempMailBox = MailBox(imap_host).login(userVar, passwordVar)
+    tempMailBox = MailBox(host=imap_host_outlook).login(userVar, passwordVar)
     # Print the login result
     if tempMailBox.login_result[0] != 'OK':
         # if login failed, print the error message
@@ -80,12 +59,15 @@ def fetch_email_with_imap_tools():
 
     # If login is successful, print the success message
     print("Login successful!")
+    wantedSubject = input("Subject (might be a keyword): ")
     print("fetching email pls wait...: " + wantedSubject)
     
-    # new method
-    # selectedMail = tempMailBox.fetch(AND(subject='Lamaran Posisi Data Scientist'))
+    # making download attachment folder
+    SAVE_DIR ="download_attachment"
+    os.makedirs(SAVE_DIR, exist_ok=True)
+    print(f"Attachments will be saved to: {os.path.abspath(SAVE_DIR)}")
+    
     for selectedMail in tempMailBox.fetch(AND(subject=wantedSubject)):
-    # for selectedMail in tempMailBox.fetch(AND(subject='Lamaran Posisi')):
         print(selectedMail.from_, selectedMail.date_str)
         # print(selectedMail.text)
         for selectedAttach in selectedMail.attachments:
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import emailFetcher as eF
 
 if __name__ == "__main__":
-    eF.fetch_email_with_imap_tools()
+    # eF.fetch_email_with_imap_tools_gmail()
+    eF.fetch_email_with_imap_tools_outlook()
     print("Done!")
